ab1d3a2b085cc8c783b2b07e0816635fb6bba8ee
Saller Thomas <thomas@saller.com.de>
2021-09-29 13:35:22 +0200

    praict in production

--- next.config.js
+++ next.config.js
@@ -1,4 +1,42 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-}
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+  eslint: {
+    dirs: ["pages", "components", "lib"],
+  },
+  experimental: { esmExternals: true },
+  webpack: (config, { dev, isServer }) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|mp4)$/i,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next",
+            name: "static/media/[name].[hash].[ext]",
+          },
+        },
+      ],
+    });
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    if (!dev && !isServer) {
+      // Replace React with Preact only in client production build
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
+});
